a770e6d774176988e27c0da172744241b6468c62
strsljennik <strsljennik@gmail.com>
2024-11-26 03:54:14 +0100

    Update formatiranje.js

--- public/formatiranje.js
+++ public/formatiranje.js
@@ -79,23 +79,10 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
-  // Povezivanje događaja za unos poruke
-document.getElementById('chatInput').addEventListener('keydown', function(event) {
-  const socket = {}; // Zamenite sa vašim socket objektom
-  const username = 'User'; // Zamenite sa stvarnim korisničkim imenom
-  const message = event.target.value;
-  const color = getDefaultColor();
-  const styles = getDefaultStyle();
-
-  if (event.key === 'Enter') {
-    handleSendMessage(socket, username, message, color, styles);
-    event.target.value = ''; // Očistiti input nakon slanja
-  }
-
   // Prikazivanje poruka sa stilovima
   socket.on('message', (data) => {
     const messageElement = document.createElement('div');
-    messageElement.textContent = `${data.username}: ${data.message} - ${data.timestamp}`; // Formatiranje sa vremenom
+    messageElement.textContent = `${data.username}: ${data.message}`;
     messageElement.style.color = data.color;  // Boja poruke
     messageElement.style.fontWeight = data.styles.bold ? 'bold' : 'normal';
     messageElement.style.fontStyle = data.styles.italic ? 'italic' : 'normal';
